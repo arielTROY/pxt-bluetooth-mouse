e756b281c94dd7afb45b3cf40a43817c40e686e7
arielT <59286427+arielTROY@users.noreply.github.com>
2023-09-08 15:28:35 +0300

    Update pxt.json, enums.d.ts, tests.ts

--- tests.ts
+++ tests.ts
@@ -1,28 +1,22 @@
-bluetooth.onBluetoothConnected(() => {
+bluetooth.onBluetoothConnected(function() {
     basic.showString("C")
 })
 
-bluetooth.onBluetoothDisconnected(() => {
+bluetooth.onBluetoothDisconnected(function() {
     basic.showString("D")
-})
 
-bluetooth.startMouseService();
+})
+bluetooth.startMouseService()
 basic.showString("S")
 
-input.onPinPressed(TouchPin.P0, () => {
+
+input.onButtonPressed(Button.A, function() {
     bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_LEFT, ButtonState.BUTTON_DOWN)
-})
-input.onPinReleased(TouchPin.P0, () => {
+    control.waitMicros(4)
     bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_LEFT, ButtonState.BUTTON_UP)
 })
 
-input.onPinPressed(TouchPin.P1, () => {
-    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_RIGHT, ButtonState.BUTTON_DOWN)
-})
-input.onPinReleased(TouchPin.P1, () => {
-    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_RIGHT, ButtonState.BUTTON_UP)
-})
-
-basic.forever(() => {
+basic.forever(function() {
     bluetooth.setMouseSpeed(input.acceleration(Dimension.X) / 8, input.acceleration(Dimension.Y) / 8, 0)
+
 })
